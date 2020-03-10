c7600275acdfb49b3478c05098f5266ac925aa4a
a1769335 <a1769335@75a19b46-2f04-421c-9d2a-529ccdbdb2b6>
2020-03-10 01:04:26 +0000

    OOP: wrote a program to perform binary arithmetic

---
+++ 2020/s1/oop/workshop-02/arithmetic.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    long long bin1,bin2;
+	int i=0,r=0;
+    int sum[20];
+	cout<<"Type 1st binary numbers : ";
+    cin>>bin1;
+    cout<<"Type 2nd binary numbers : ";
+    cin>>bin2;
+    while (bin1 != 0 || bin2 !=0){
+        sum[i++] =(int)((bin1%10+bin2%10+r)%2);
+        r = (int)((bin1%10+bin2%10+r)/2);
+        bin1=bin1/10;
+        bin2=bin2/10;
+    }
+    if (r !=0){
+        sum[i++]=r;
+    }
+    --i;
+    cout << "The sum of binary numbers is: " ;
+    while(i>=0){
+        cout<<(sum[i--]);
+    }
+}
